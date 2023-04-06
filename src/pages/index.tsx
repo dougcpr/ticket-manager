1a4458d775debf520686036fcf0f8cea7fa88d81
dougcpr <doug.cpr@gmail.com>
2023-04-06 12:37:05 -0700

    fixed flashing of unstyled content by adding a loading div before the session was fully recognized

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,31 +2,33 @@ import React, {useEffect, useState} from "react";
 import {Session, useSupabaseClient} from '@supabase/auth-helpers-react'
 import SignIn from "@/pages/sign-in";
 import {AuthChangeEvent} from "@supabase/gotrue-js";
-const Index = ({children}: any) => {
+const Index = () => {
   const [session, setSession] = useState<Session | null>(null)
+  const [isLoading, setLoading] = useState(true)
   const supabase = useSupabaseClient()
   useEffect(() => {
     supabase?.auth?.getSession().then(({ data: { session} }: any ) => {
       setSession(session)
     })
-
     const {
       data: { subscription },
     } = supabase?.auth?.onAuthStateChange((_event: AuthChangeEvent, session: Session | null) => {
       setSession(session)
+      setLoading(false)
     })
-
     return () => subscription.unsubscribe()
   }, [])
 
+  if (isLoading) return <div />
+
   return (
-    <div>
+    <>
       {!session ? (
         <SignIn setSession={setSession} />
       ) : (
         <>Welcome to your account.</>
       )}
-    </div>
+    </>
   )
 }
 
